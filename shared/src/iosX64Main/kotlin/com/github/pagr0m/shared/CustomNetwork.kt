f39f5ca728f527b8cb55fc5d291e0c1d9e75bc6c
pagr0m <pagrom27@gmail.com>
2021-05-31 21:52:24 +0200

    Need add CompletionHandler

--- shared/src/iosX64Main/kotlin/com/github/pagr0m/shared/CustomNetwork.kt
+++ shared/src/iosX64Main/kotlin/com/github/pagr0m/shared/CustomNetwork.kt
@@ -1,31 +1,36 @@
 package com.github.pagr0m.shared
 
 import platform.Foundation.NSError
-import platform.Foundation.NSProgress
 import platform.Foundation.NSURLSessionDataTask
 import spm.AFNetworking.AFHTTPSessionManager
 
 actual class CustomNetwork actual constructor() {
     actual fun get(url: String): String {
+        var response = "empty"
         val manager = AFHTTPSessionManager()
 
-        manager.GET(
+        val sessionDataTask = manager.GET(
             url,
             parameters = null,
             headers = null,
-            progress = { nsProgress: NSProgress? ->
-                println("progress: ${nsProgress.toString()}")
+            progress = null,
+            success = { _: NSURLSessionDataTask?, responseObject: Any? ->
+                response = responseObject.toString()
+                println(responseObject.toString())
             },
-            success = { nsurlSessionDataTask: NSURLSessionDataTask?, any: Any? ->
-                println("success: ${nsurlSessionDataTask.toString()}")
-                println("success: ${any.toString()}")
-            },
-            failure = { nsurlSessionDataTask: NSURLSessionDataTask?, nsError: NSError? ->
-                println("failure: ${nsurlSessionDataTask.toString()}")
-                println("failure: ${nsError.toString()}")
+            failure = { _: NSURLSessionDataTask?, error: NSError? ->
+                println(error.toString())
             }
         )
 
-        return "null"
+        println("Check0: " + response)
+
+        println("Check1: " + sessionDataTask?.response.toString())
+        println("Check2: " + sessionDataTask?.currentRequest().toString())
+        println("Check3: " + sessionDataTask?.description)
+        println("Check4: " + sessionDataTask?.response())
+        response = sessionDataTask?.response.toString()
+
+        return response
     }
 }
